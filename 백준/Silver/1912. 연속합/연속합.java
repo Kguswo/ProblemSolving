0b9c633bea6e21c21e5bac3cba3b8a14b35d9195
Now <156046839+Kguswo@users.noreply.github.com>
2024-03-12 02:51:45 +0900

    [Silver II] Title: 연속합, Time: 760 ms, Memory: 101688 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,23 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-	static int N, currentsum, maxsum;
-	static int[] arr;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		arr = new int[N+1];
-		
-		for(int i=1; i<=N; i++) {
-			arr[i] = sc.nextInt();
-		}
-		currentsum = arr[0];
-		maxsum = arr[1];
+    static int N, maxsum;
+    static int[] arr;
 
-		for(int i=1; i<=N; i++) {
-			currentsum = Math.max(arr[i], currentsum+arr[i]);
-			maxsum = Math.max(maxsum, currentsum);
-		}
-		System.out.println(maxsum);
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int ans = maxsum(arr);
+        System.out.println(ans);
+    }
+
+    private static int maxsum(int[] arr) {
+        maxsum = arr[0];
+        int currentsum = 0;
+        int left = 0;
+
+        int right = 0;
+        while (right < N) {
+            currentsum += arr[right];
+            maxsum = Math.max(maxsum, currentsum);
+
+            if (currentsum < 0) { // 합이 음수일 때
+                currentsum = 0;
+                left = right + 1;
+            }
+
+            right++;
+        }
+
+        return maxsum;
+    }
 }
