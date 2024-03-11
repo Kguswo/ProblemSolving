d98e22a2e1dadb0a0e62103c8f625a857efbd817
Now <156046839+Kguswo@users.noreply.github.com>
2024-03-12 02:49:00 +0900

    [Silver II] Title: 연속합, Time: 792 ms, Memory: 108964 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 	static int N, currentsum, maxsum;
-	static int[] arr, sum;
+	static int[] arr;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		arr = new int[N+1];
-		sum = new int[N+1];
 		
 		for(int i=1; i<=N; i++) {
 			arr[i] = sc.nextInt();
