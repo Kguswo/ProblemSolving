b2384ac53ba2172c129efc090986f6ed3f6ef0e6
Now <156046839+Kguswo@users.noreply.github.com>
2024-03-20 04:19:12 +0900

    [Gold V] Title: 퇴사 2, Time: 2524 ms, Memory: 346308 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static int [][] board;
+	static int [] dp;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N=sc.nextInt();
+		board = new int[2][N];
+		dp = new int [N+1];
+		for(int i=0; i<N; i++) {
+			board[0][i] = sc.nextInt();
+			board[1][i] = sc.nextInt();
+		}
+		for(int i=N-1; i>=0; i--) {
+			if(i+board[0][i]<=N) dp[i]=Math.max(dp[board[0][i]+i]+board[1][i], dp[i+1]);
+			else dp[i] = dp[i+1];
+		}
+		System.out.println(dp[0]);
+	}
+}
