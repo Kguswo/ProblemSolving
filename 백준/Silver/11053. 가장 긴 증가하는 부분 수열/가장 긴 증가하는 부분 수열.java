123bf76916042b21ec8ebf2484074625cc9b3125
Now <156046839+Kguswo@users.noreply.github.com>
2024-03-20 01:38:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 288 ms, Memory: 20860 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -8,18 +8,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N=sc.nextInt();
-		arr = new int[N+1];
-		dp = new int[N+1];
-		for(int i=1; i<=N; i++) {
+		arr = new int[N];
+		dp = new int[N];
+		for(int i=0; i<N; i++) {
 			arr[i] = sc.nextInt();
 			dp[i] = 1;
 		}
 		DP(dp);
 	}
 	private static void DP(int[] dp) {
-		for(int i=2; i<=N; i++) {
-			for(int j=1; j<=i-1; j++) {
-				if(arr[i]>arr[i-j]) dp[i] = Math.max(dp[i-j] + 1, dp[i]); //바로 전 작은값까지 최대길이 + 1로 마무리
+		for(int i=1; i<N; i++) {
+			for(int j=0; j<i; j++) {
+				if(arr[i]>arr[j]) dp[i] = Math.max(dp[j] + 1, dp[i]); //바로 전 작은값까지 최대길이 + 1로 마무리
 			}
 		}
 		for(int i : dp) ans = Math.max(i, ans);
