ac28979ba471b6094cf6e76b925d2b3b23d42142
Now <156046839+Kguswo@users.noreply.github.com>
2024-03-23 16:43:53 +0900

    [Bronze II] Title: 나머지, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [] count = new int [42];
+		for(int i=0; i<10; i++) {
+			int num = sc.nextInt();
+			count[num%42]++;
+		}
+		int ans = 0;
+		for(int i=0; i<42; i++) {
+			if(count[i] != 0 ) ans ++;
+		}
+		System.out.println(ans);
+	}
+}
