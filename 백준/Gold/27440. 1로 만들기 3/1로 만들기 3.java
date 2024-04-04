e4d8ef4b44f9fc6a6fae889d5c5b1eaa33040051
Now <156046839+Kguswo@users.noreply.github.com>
2024-04-05 05:03:41 +0900

    [Gold IV] Title: 1로 만들기 3, Time: 260 ms, Memory: 21560 KB -BaekjoonHub

--- 백준/Gold/27440. 1로 만들기 3/1로 만들기 3.java
+++ 백준/Gold/27440. 1로 만들기 3/1로 만들기 3.java
@@ -1,6 +1,6 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -14,12 +14,12 @@ public class Main {
     }
 
     private static long bfs(long N) {
-        Deque<long[]> dq = new ArrayDeque<>();
-        dq.offer(new long[]{N, 0});
+        Queue<long[]> queue = new LinkedList<long[]>();
+        queue.offer(new long[]{N, 0});
         Set<Long> visited = new HashSet<>();
 
-        while (!dq.isEmpty()) {
-            long[] arr = dq.poll();
+        while (!queue.isEmpty()) {
+            long[] arr = queue.poll();
             long idx = arr[0];
             long cnt = arr[1];
 
@@ -29,15 +29,15 @@ public class Main {
 
             if (!visited.contains(idx - 1)) {
                 visited.add(idx - 1);
-                dq.offer(new long[]{idx - 1, cnt + 1});
+                queue.offer(new long[]{idx - 1, cnt + 1});
             }
             if (idx % 3 == 0 && !visited.contains(idx / 3)) {
                 visited.add(idx / 3);
-                dq.offer(new long[]{idx / 3, cnt + 1});
+                queue.offer(new long[]{idx / 3, cnt + 1});
             }
             if (idx % 2 == 0 && !visited.contains(idx / 2)) {
                 visited.add(idx / 2);
-                dq.offer(new long[]{idx / 2, cnt + 1});
+                queue.offer(new long[]{idx / 2, cnt + 1});
             }
         }
         return Integer.MIN_VALUE;
