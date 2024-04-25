05dbfd8a38652ecc3425f0a036dd28fa6df3b7d4
Now <156046839+Kguswo@users.noreply.github.com>
2024-04-26 01:26:53 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 256 ms, Memory: 19964 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -27,6 +27,8 @@ public class Main {
         }
 
         dp = new int[61][61][61];
+        dp[0][0][0] = 0;
+
         for (int i = 0; i <= scv[0]; i++) {
             for (int j = 0; j <= scv[1]; j++) {
                 for (int k = 0; k <= scv[2]; k++) {
