7a41bb13a9e87b730bbdc5c620c1730f7ba9d1d1
Now <156046839+Kguswo@users.noreply.github.com>
2024-04-28 21:36:06 +0900

    [Silver V] Title: 단어 정렬, Time: 880 ms, Memory: 60500 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,38 +1,42 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
- 
- 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
     
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
  
-		int N = sc.nextInt();
-		String[] arr = new String[N];
+        int N = sc.nextInt();
+        String[] arr = new String[N];
  
-		sc.nextLine();
+        sc.nextLine();
  
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextLine();
-		}
-		
-		Arrays.sort(arr, new Comparator<String>() {
-			public int compare(String o1, String o2) {
-				
-				if (o1.length() == o2.length()) {
-					return o1.compareTo(o2);
-				} 
-				else {
-					return o1.length() - o2.length();
-				}
-			}
-		}); 
-		System.out.println(arr[0]);
-		
-		for (int i = 1; i < N; i++) {
-			if (!arr[i].equals(arr[i - 1])) System.out.println(arr[i]);
-		}
-	}
- 
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextLine();
+        }
+        
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } 
+                else {
+                    return o1.length() - o2.length();
+                }
+            }
+        }); 
+        
+        Set<String> set = new LinkedHashSet<>();
+        for (String s : arr) {
+            set.add(s);
+        }
+        
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
 }
