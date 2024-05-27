faad684abbf6b79238c5000579c9ffec52b494b6
Now <156046839+Kguswo@users.noreply.github.com>
2024-05-28 03:31:45 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.04 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/README.md
+++ 프로그래머스/1/134240. 푸드 파이트 대회/README.md
@@ -1,10 +1,10 @@
 # [level 1] 푸드 파이트 대회 - 134240 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240) 
 
 ### 성능 요약
 
-메모리: 79.8 MB, 시간: 9.75 ms
+메모리: 86 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 28일 03:24:02
+2024년 05월 28일 03:33:08
 
 ### 문제 설명
 
--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,17 +1,11 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        for(int i=1; i<food.length; i++){
-            for(int j=0; j<food[i]/2; j++){
-                answer += i;
-            }
-        }
-        answer += "0";
-        for(int i=food.length-1; i>=1; i--){
-            for(int j=0; j<food[i]/2; j++){
-                answer += i;
-            }
-        }   
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            answer.append(String.valueOf(i).repeat(food[i] / 2));
+        }       
+        String left = answer.toString();
+        answer.append("0").append(new StringBuilder(left).reverse());
+        return answer.toString();
     }
 }
