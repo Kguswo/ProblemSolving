9a8238c3ae065a0525eaf8c37c00ecf06ed81053
Now <156046839+Kguswo@users.noreply.github.com>
2024-06-12 04:46:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.82 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,28 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        // 맵의 크기
         int n = maps.length, m = maps[0].length;
-        // 방문 배열을 맵의 크기보다 2씩 더 크게 만들어서 경계를 0으로 설정
-        int[][] visited = new int[n+2][m+2];
-        bfs(maps, visited, n, m);
-        int answer = visited[n-1][m-1];
+        
+        // 경계를 0으로 둘러싸기 위해 새로운 맵 생성
+        int[][] extendedMaps = new int[n + 2][m + 2];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                extendedMaps[i + 1][j + 1] = maps[i][j];
+            }
+        }
+        
+        // 방문 배열 생성
+        int[][] visited = new int[n + 2][m + 2]; // 0으로 초기화
+        
+        // 1,1부터 출발하고, n, m을 출력해주면 되므로 한번만 호출
+        bfs(extendedMaps, visited, n + 2, m + 2);
+        int answer = visited[n][m];
 
         return (answer > 0) ? answer : -1;
     }
 
-    // 이동 방향 (상, 하, 좌, 우)
-    static int[][] d = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    
-    // 최단 거리를 구해야 하므로 BFS 사용
+    static int[][] d = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    // 최단 거리이므로 BFS 사용
     public static void bfs(int[][] maps, int[][] visited, int n, int m) {
-        // 시작 위치
-        int x = 0, y = 0;
+        int x = 1, y = 1;
         visited[y][x] = 1;
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{y, x});
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int[] now = queue.poll();
             int r = now[0];
             int c = now[1];
@@ -32,13 +40,9 @@ class Solution {
                 int dr = r + d[i][0];
                 int dc = c + d[i][1];
 
-                // 경계를 넘어가는지 확인
-                if (dr < 0 || dr >= n || dc < 0 || dc >= m) {
-                    continue;
-                }
-                // 방문하지 않았고, 벽이 아닌 경우
+                // 경계를 넘어가는지 확인할 필요가 없음
                 if (visited[dr][dc] == 0 && maps[dr][dc] == 1) {
-                    queue.add(new int[] {dr, dc});
+                    queue.add(new int[]{dr, dc});
                     visited[dr][dc] = visited[r][c] + 1;
                 }
             }
