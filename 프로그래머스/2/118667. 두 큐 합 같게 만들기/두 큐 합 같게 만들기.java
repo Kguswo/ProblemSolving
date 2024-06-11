59b2e9f124c1db49ef1e6787562382d29b2680da
Now <156046839+Kguswo@users.noreply.github.com>
2024-06-12 04:07:29 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 49.90 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,41 +1,40 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
-    public int solution(int[] queue1, int[] queue2) {
+    public int solution(int[] arr1, int[] arr2) {
         int answer = 0;
-
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
+        
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue2 = new LinkedList<>();
         long sum1 = 0;
         long sum2 = 0;
 
-        for (int i : queue1) {
-            q1.offer(i);
+        for (int i : arr1) {
+            queue1.offer(i);
             sum1 += i;
         }
 
-        for (int i : queue2) {
-            q2.offer(i);
+        for (int i : arr2) {
+            queue2.offer(i);
             sum2 += i;
         }
 
-        int size1 = q1.size();
-        int size2 = q2.size();
+        int size1 = queue1.size();
+        int size2 = queue2.size();
 
-        while (!q1.isEmpty() && !q2.isEmpty()) {
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
             if (sum1 == sum2) return answer;
 
             if (sum1 > sum2) {
                 size1--;
-                int popped = q1.poll();
-                q2.offer(popped);
+                int popped = queue1.poll();
+                queue2.offer(popped);
                 sum1 -= popped;
                 sum2 += popped;
             } else {
                 size2--;
-                int popped = q2.poll();
-                q1.offer(popped);
+                int popped = queue2.poll();
+                queue1.offer(popped);
                 sum2 -= popped;
                 sum1 += popped;
             }
