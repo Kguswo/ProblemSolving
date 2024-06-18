024cae1dcaf697d77ab574e8807456a25dbec648
Now <156046839+Kguswo@users.noreply.github.com>
2024-06-18 13:31:55 +0900

    [level 2] Title: 구명보트, Time: 23.52 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,24 +1,19 @@
 import java.util.*;
 
 class Solution {
+    static List<Integer> list;
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        List<Integer> list = new ArrayList<>();
-        
-        for (int a : people) {
-            list.add(a);
-        }
-
+        list = new ArrayList<>();
+        for (int a : people) list.add(a); 
         while (!list.isEmpty()) {
-            int lastnum = list.get(list.size() - 1); // 맨 뒤에 있는 사람의 몸무게
-            if (list.size() > 1 && list.get(0) + list.get(list.size() - 1) <= limit) { // 젤 가벼운애 한명 더 태울 수 있으면 같이태움
+            if (list.size() >= 2 && list.get(0) + list.get(list.size() - 1) <= limit) { // 젤 가벼운애 한명 더 태울 수 있으면 같이태움
                 list.remove(0);
                 list.remove(list.size() - 1);
             } else list.remove(list.size() - 1); // 한명만 태울때
             answer++;
         }
-
         return answer;
     }
 }
