845a47168a0c9291d2d1fe155376663f9287f882
Now <156046839+Kguswo@users.noreply.github.com>
2024-06-26 13:15:41 +0900

    [level 2] Title: 더 맵게, Time: 1261.12 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,22 +1,6 @@
-import java.util.*;
-
 class Solution {
-    static PriorityQueue<Integer> queue = new PriorityQueue<>();
-    static int mix;
     public int solution(int[] scoville, int K) {
-        mix = 0;
-        for (int a : scoville) queue.add(a);
-        
-        while (queue.size() >= 2) {
-            if (queue.peek() >= K) return mix;
-
-            int first = queue.poll();
-            int second = queue.poll();
-            int newfood = first + (second * 2);
-            queue.add(newfood);
-            mix++;
-        }
-        if (queue.peek() >= K) return mix;
-        return -1;
+        int answer = 0;
+        return answer;
     }
 }
