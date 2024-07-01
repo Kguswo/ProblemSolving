e00c782a22e91a64efcda218929112f8fb4ccef3
Now <156046839+Kguswo@users.noreply.github.com>
2024-07-02 03:51:22 +0900

    [Gold V] Title: A와 B 2, Time: 168 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+	static String S, T;
+    static boolean Same = false;
+    public static void main(String[] args) throws FileNotFoundException {
+
+    	Scanner sc = new Scanner(System.in);
+        S = sc.next();
+        T = sc.next();
+
+        TtoS(S, T, T.length() - 1);
+        System.out.println(Same ? 1 : 0);
+    }
+
+    private static void TtoS(String S, String T, int end) {
+        if (Same) return;
+        
+        if (S.length() == T.length()) { // 체크
+            if (S.equals(T)) Same = true;
+            return;
+        }
+        
+        // A제거
+        if (T.charAt(end) == 'A') TtoS(S, T.substring(0, end), end - 1);
+        
+        // 뒤집고 B제거
+        if (new StringBuilder(T).reverse().toString().charAt(end) == 'B') TtoS(S, new StringBuilder(T).reverse().substring(0, end), end - 1);
+    }
+    
+//    private static String reverse(String T) {
+//        String newT = "";
+//        for (int i = T.length() - 1; i >= 0; i--) {
+//        	newT = newT + T.charAt(i);        
+//        }
+//		return newT;
+//    }
+}
