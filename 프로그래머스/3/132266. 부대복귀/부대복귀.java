056bf9050eb57023b22dcc52da4015c0bfa1f708
Now <156046839+Kguswo@users.noreply.github.com>
2024-07-02 21:33:00 +0900

    [level 3] Title: 부대복귀, Time: 63.20 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,10 +1,16 @@
+// 거꾸로 통과(거리 저장)
 import java.util.*;
 
 class Solution {
+    static List<Integer>[] linked;
+    static int[] minlen;
     static int N, goal;
+
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        List<Integer>[] linked = new List[n + 1];
-        N = n; goal = destination;
+        linked = new ArrayList[n + 1];
+        N = n;
+        goal = destination;
+        
         for (int i = 1; i <= n; i++) {
             linked[i] = new ArrayList<>();
         }
@@ -13,29 +19,36 @@ class Solution {
             linked[r[0]].add(r[1]);
             linked[r[1]].add(r[0]);
         }
-        
-        int[] minlen = new int[n + 1];
-        Arrays.fill(minlen, -1);
-        minlen[goal] = 0;
-        
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(goal);
-        
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            for (int i : linked[current]) {
-                if (minlen[i] == -1) {
-                    minlen[i] = minlen[current] + 1;
-                    queue.offer(i);
-                }
-            }
-        }
+
+        minlen = new int[N + 1];
+        bfs(goal);
         
         int[] answer = new int[sources.length];
         for (int i = 0; i < sources.length; i++) {
             answer[i] = minlen[sources[i]];
         }
-        
         return answer;
+    }
+    
+    private static void bfs(int end) {
+        for(int i=1; i<=N; i++){
+            if(i==end) minlen[i] = 0;
+            else minlen[i] = -1;
+        }
+        // minlen[end] = 0;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(end);
+        
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            
+            for (int a : linked[cur]) {
+                if (minlen[a] == -1) {
+                    minlen[a] = minlen[cur] + 1;
+                    queue.add(a);
+                }
+            }
+        }
     }
 }
