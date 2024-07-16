71866e2e1f2c831626032d5536171049ee19cb08
Now <156046839+Kguswo@users.noreply.github.com>
2024-07-17 05:44:27 +0900

    [level 3] Title: 단어 변환, Time: 1.57 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
         if (!Arrays.asList(words).contains(target)) return 0;
 
+        Set<String> visited = new HashSet<>();
         Queue<String> queue = new LinkedList<>();
         Queue<Integer> countQueue = new LinkedList<>();
         queue.offer(begin);
@@ -17,26 +19,23 @@ class Solution {
                 return count;
             }
 
-            for (String word : words) {
-                if (canConvert(current, word)) {
-                    queue.offer(word);
-                    countQueue.offer(count + 1);
-                }
+            List<String> nextWords = Arrays.stream(words)
+                                           .filter(word -> canConvert(current, word) && !visited.contains(word))
+                                           .collect(Collectors.toList());
+
+            for (String word : nextWords) {
+                queue.offer(word);
+                countQueue.offer(count + 1);
+                visited.add(word);
             }
         }
         return 0;
     }
 
     private boolean canConvert(String word1, String word2) {
-        int diffCount = 0;
-        for (int i = 0; i < word1.length(); i++) {
-            if (word1.charAt(i) != word2.charAt(i)) {
-                diffCount++;
-            }
-            if (diffCount > 1) {
-                return false;
-            }
-        }
+        long diffCount = IntStream.range(0, word1.length())
+                                  .filter(i -> word1.charAt(i) != word2.charAt(i))
+                                  .count();
         return diffCount == 1;
     }
 }
