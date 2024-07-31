21d710bd3eab5cbd2faa1e42f7950eaa164c0d34
Now <156046839+Kguswo@users.noreply.github.com>
2024-07-31 22:25:50 +0900

    [Bronze IV] Title: FA, Time: 124 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/14935. FA/FA.java
+++ 백준/Bronze/14935. FA/FA.java
@@ -1,8 +1,5 @@
-import java.util.*;
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String x = sc.nextLine();
+class Main{
+	public static void main(String[] args){
 		System.out.print("FA");
 	}
 }
