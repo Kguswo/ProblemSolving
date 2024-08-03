d3b0048b5605d4fd10fb856539d322c3d7c6b1d6
Now <156046839+Kguswo@users.noreply.github.com>
2024-08-03 23:37:44 +0900

    [Bronze IV] Title: R2, Time: 180 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int s = sc.nextInt();
+        System.out.println(2*s - R1);
+    }
+}
