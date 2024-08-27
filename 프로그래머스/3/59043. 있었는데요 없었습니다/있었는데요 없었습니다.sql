757d82a0c54faa25e24a72e95500f6135d0ba5ef
Now <156046839+Kguswo@users.noreply.github.com>
2024-08-28 02:41:55 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,2 @@
-SELECT ai.ANIMAL_ID, ai.NAME
-    FROM ANIMAL_INS AS ai
-        INNER JOIN ANIMAL_OUTS AS ao
-            ON ao.ANIMAL_ID = ai.ANIMAL_ID
-                WHERE ai.DATETIME > ao.DATETIME
-                    ORDER BY ai.DATETIME ASC;
+-- 코드를 입력하세요
+SELECT
