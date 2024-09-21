8eccd8081470993399bfce79091c9d2668e84a34
Now <156046839+Kguswo@users.noreply.github.com>
2024-09-21 10:12:06 +0900

    [Gold II] Title: 불켜기, Time: 248 ms, Memory: 26468 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.java
+++ 백준/Gold/11967. 불켜기/불켜기.java
@@ -1,35 +1,26 @@
-
-/**
- * Author: nowalex322, Kim HyeonJae
- */
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] dr = { 0, 0, 1, -1 };
+    static int[] dc = { 1, -1, 0, 0 };
+    private static final int LIGHT_ON = 1;
+    private static final int VISITED = 2;
+    private static final int CAN_VISIT = 3;
 
-	static BufferedReader br;
-	static BufferedWriter bw;
-	static StringTokenizer st;
-	static int[] dr = { 0, 0, 1, -1 };
-	static int[] dc = { 1, -1, 0, 0 };
-	private static final int LIGHT_ON = 1;
-	private static final int VISITED = 2;
-    private static final int CANDIDATE = 3;
-    
-	public static void main(String[] args) throws Exception {
-		new Main().solution();
-	}
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
 
-	public void solution() throws Exception {
-		br = new BufferedReader(new InputStreamReader(System.in));
-//		br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		Map<Area, List<Area>> map = new HashMap<>();
-		
-		while (m-- > 0) {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Map<Area, List<Area>> map = new HashMap<>();
+
+        while (m-- > 0) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
@@ -38,10 +29,17 @@ public class Main {
             Area cur = new Area(x, y);
             map.computeIfAbsent(cur, k -> new ArrayList<>()).add(new Area(a, b));
         }
-		
-		Queue<Area> queue = new ArrayDeque<>();
-        int[][] v = new int[n+1][n+1];
-        v[1][1] = VISITED;
+
+        int result = bfs(n, map);
+        System.out.println(result);
+
+        br.close();
+    }
+
+    private int bfs(int n, Map<Area, List<Area>> map) {
+        Queue<Area> queue = new ArrayDeque<>();
+        int[][] board = new int[n+1][n+1];
+        board[1][1] = VISITED;
         queue.add(new Area(1, 1));
         int cnt = 1;
 
@@ -49,13 +47,13 @@ public class Main {
             Area current = queue.poll();
             if (map.containsKey(current)) {
                 for (Area light : map.get(current)) {
-                    if (v[light.r][light.c] == VISITED || v[light.r][light.c] == LIGHT_ON) continue;
+                    if (board[light.r][light.c] == VISITED || board[light.r][light.c] == LIGHT_ON) continue;
                     cnt++;
-                    if (v[light.r][light.c] == CANDIDATE) {
+                    if (board[light.r][light.c] == CAN_VISIT) {
                         queue.add(light);
-                        v[light.r][light.c] = VISITED;
+                        board[light.r][light.c] = VISITED;
                     } else {
-                        v[light.r][light.c] = LIGHT_ON;
+                        board[light.r][light.c] = LIGHT_ON;
                     }
                 }
             }
@@ -63,40 +61,39 @@ public class Main {
             for (int d = 0; d < 4; d++) {
                 int nr = current.r + dr[d];
                 int nc = current.c + dc[d];
-                if (nr < 1 || nr > n || nc < 1 || nc > n || v[nr][nc] == VISITED || v[nr][nc] == CANDIDATE) continue;
+                if (nr < 1 || nr > n || nc < 1 || nc > n || board[nr][nc] == VISITED || board[nr][nc] == CAN_VISIT) continue;
 
-                if (v[nr][nc] == LIGHT_ON) {
-                    v[nr][nc] = VISITED;
+                if (board[nr][nc] == LIGHT_ON) {
+                    board[nr][nc] = VISITED;
                     queue.add(new Area(nr, nc));
                     continue;
                 }
 
-                v[nr][nc] = CANDIDATE;
+                board[nr][nc] = CAN_VISIT;
             }
         }
-        System.out.println(cnt);
-
-        br.close();
+        return cnt;
     }
 }
 
-
 class Area {
-	int r, c;
+    int r, c;
 
-	public Area(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
+    public Area(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
 
-	@Override
-	public int hashCode() {
-		return c * 20000 + r;
-	}
+    @Override
+    public int hashCode() {
+        return c * 20000 + r;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		Area a = (Area) o;
-		return this.r == a.r && this.c == a.c;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Area a = (Area) o;
+        return this.r == a.r && this.c == a.c;
+    }
 }
