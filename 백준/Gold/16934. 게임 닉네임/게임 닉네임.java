d3608e1a77aebba282e41ba389c7a10548cb67d3
Now <156046839+Kguswo@users.noreply.github.com>
2024-09-22 20:15:38 +0900

    [Gold III] Title: 게임 닉네임, Time: 404 ms, Memory: 71332 KB -BaekjoonHub

--- 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
@@ -1,79 +1,66 @@
-/**
- * Author: nowalex322, Kim HyeonJae
- */
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static BufferedReader br;
-	static BufferedWriter bw;
-	static StringTokenizer st;
+    static BufferedReader br;
+    static StringBuilder result;
+    static Trie root;
 
-	public static void main(String[] args) throws Exception {
-		new Main().solution();
-	}
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        result = new StringBuilder();
+        root = new Trie();
 
-	static Trie root;
+        int N = Integer.parseInt(br.readLine());
 
-	public void solution() throws Exception {
-		br = new BufferedReader(new InputStreamReader(System.in));
-//		br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+            insertName(name);
+        }
 
-		int N = Integer.parseInt(br.readLine());
-		root = new Trie();
-		
-		for (int i = 0; i < N; i++) {
-			String name = br.readLine();
-			insertName(name);
-		}
+        System.out.print(result.toString());
+    }
 
+    private static void insertName(String name) {
+        Trie trie = root;
+        StringBuilder sb = new StringBuilder();
+        boolean isNewBranch = false;
 
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        for (int i = 0; i < name.length(); i++) {
+            int idx = name.charAt(i) - 'a';
+            if (!isNewBranch) sb.append(name.charAt(i));
 
-	private void insertName(String name) throws IOException {
-		Trie trie = root;
-		StringBuilder sb = new StringBuilder();
-		// 새로운 분기인지 판별 
-		boolean flag = false;
-		
-		for (int i = 0; i < name.length(); i++) {
-			int idx = name.charAt(i) - 'a';
-			if(!flag) sb.append(name.charAt(i));
-			
-			// 새로 분기를 만들어야 하면 생성하고 별칭에 추가. 
-			if (trie.children[idx] == null) {
-				flag = true;
-				trie.children[idx] = new Trie();
-			}
+            if (trie.children == null) {
+                trie.children = new Trie[26];
+            }
 
-			trie = trie.children[idx];
-		}
-		
-		trie.isEnd = true;
-			if(trie.count == 0)  trie.count++;
-			else {
-				String x = String.valueOf(++trie.count);
-				sb.append(x);
-			}
-		bw.write(sb.toString() + "\n");
-	}
+            if (trie.children[idx] == null) {
+                isNewBranch = true;
+                trie.children[idx] = new Trie();
+            }
 
+            trie = trie.children[idx];
+        }
+
+        if (trie.isEnd) {
+            sb.append(++trie.count);
+        } else {
+            trie.isEnd = true;
+            trie.count = 1;
+        }
+
+        result.append(sb.toString()).append("\n");
+    }
 }
 
 class Trie {
-	Trie[] children = new Trie[26];
-	int count;
-	boolean isEnd;
+    Trie[] children;
+    int count;
+    boolean isEnd;
 
-	Trie() {
-		this.isEnd = false;
-		for (int i = 0; i < 26; i++) {
-			children[i] = null;
-		}
-		this.count = 0;
-	}
+    Trie() {
+        this.children = null;
+        this.count = 0;
+        this.isEnd = false;
+    }
 }
