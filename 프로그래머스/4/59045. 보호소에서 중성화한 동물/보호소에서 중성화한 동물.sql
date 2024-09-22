366cdd4e56066d0e38e4b50c4b62825fef9c8454
Now <156046839+Kguswo@users.noreply.github.com>
2024-09-22 18:29:42 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,8 +1,10 @@
 SELECT ao.ANIMAL_ID, ao.ANIMAL_TYPE, ao.NAME
-    FROM ANIMAL_OUTS AS ao
-        JOIN ANIMAL_INS AS ai
-            ON ai.ANIMAL_ID = ao.ANIMAL_ID
-                WHERE ai.SEX_UPON_INTAKE LIKE 'Intact%'
-                    AND (ao.SEX_UPON_OUTCOME LIKE 'Spayed%' OR ao.SEX_UPON_OUTCOME LIKE 'Neutered%')
-                        ORDER BY ao.ANIMAL_ID ASC;
-    
+FROM ANIMAL_OUTS ao
+WHERE EXISTS (
+    SELECT 1
+    FROM ANIMAL_INS ai
+    WHERE ai.ANIMAL_ID = ao.ANIMAL_ID
+    AND ai.SEX_UPON_INTAKE LIKE 'Intact%'
+)
+AND (ao.SEX_UPON_OUTCOME LIKE 'Spayed%' OR ao.SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY ao.ANIMAL_ID ASC;
