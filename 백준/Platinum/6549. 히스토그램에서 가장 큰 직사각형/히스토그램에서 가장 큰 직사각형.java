a7d47167b0c6b10f98ea1fec29d8ce4d2c22a13f
Now <156046839+Kguswo@users.noreply.github.com>
2024-09-30 03:00:05 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 968 ms, Memory: 56948 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -0,0 +1,102 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static int n, tree[], num[];
+	
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
+
+	public void solution() throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+//		br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			if(n==0) break;
+			
+			num = new int[n+1];
+			for(int i=1; i<=n; i++) {
+				num[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			tree = new int[4*n];
+			init(1, n, 1);
+			
+			long maxRecS = getMaxRectangle(1, n);
+			sb.append(maxRecS).append("\n");
+		}
+		
+        bw.write(sb.toString());
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	/**
+	 * a~b 까지 최대 직사각형 S 계산 후
+	 * 좌우 분할정복으로 다시 계산 ( 다이아몬드 모양 )
+	 * 
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	private long getMaxRectangle(int left, int right) {
+        if (left > right) return 0;
+
+		long maxS = 0L;
+		long tmpS = 0L;
+		int idx = getMinH(1, n, 1, left, right);
+		
+		maxS = (long)(right-left+1) * (long)num[idx];
+
+		tmpS = getMaxRectangle(left, idx-1);
+        maxS = Math.max(maxS, tmpS);
+        
+        tmpS = getMaxRectangle(idx+1, right);
+        maxS = Math.max(maxS, tmpS);
+        
+		return maxS;
+		
+	}
+
+    private int getMinH(int start, int end, int node, int left, int right) {
+        if(left > end || right < start) return 0;
+        
+        if(left <= start && end <= right) return tree[node];
+        
+        int mid = (start + end) / 2;
+        int leftIdx = getMinH(start, mid, node*2, left, right);
+        int rightIdx = getMinH(mid+1, end, node*2+1, left, right);
+        
+        if(leftIdx == 0) return rightIdx;
+        if(rightIdx == 0) return leftIdx;
+        return num[leftIdx] < num[rightIdx] ? leftIdx : rightIdx;
+    }
+
+	// 각 노드마다 구간별 최소높이의 인덱스를 저장 ( 높이 저장하면 분할정복 불가능 )
+	private int init(int start, int end, int node) {
+		if(start == end) return tree[node] = start;
+		
+		int mid = (start + end) / 2;
+		int left = init(start, mid, node*2);
+		int right = init(mid+1, end, node*2+1);
+		
+		return tree[node] = num[left] < num[right] ? left : right;
+	}
+	
+	
+}
