51676ffca09f00472819fedb70ebcaefcf1bf1d7
Now <156046839+Kguswo@users.noreply.github.com>
2024-10-05 23:54:59 +0900

    [Bronze III] Title: 택시 기하학, Time: 100 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,17 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+import java.io.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		double d = Double.parseDouble(br.readLine());
+	
+		System.out.println(d * d * Math.PI);	// 유클리드 원
+		System.out.println(2 * d * d);		// 택시기하학 원
+		
+	}
+}
