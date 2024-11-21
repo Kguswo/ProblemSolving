4142f5c947bf2987881c88043dc38885299ba739
Now <156046839+Kguswo@users.noreply.github.com>
2024-11-21 18:01:47 +0900

    [Silver I] Title: 신입 사원, Time: 868 ms, Memory: 300128 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -7,7 +7,6 @@ import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -27,29 +26,53 @@ public class Main {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         T = Integer.parseInt(br.readLine());
+//        while (T-- > 0) {
+//            N = Integer.parseInt(br.readLine());
+//            arr = new int[N][2];
+//            for (int i = 0; i < N; i++) {
+//                st = new StringTokenizer(br.readLine());
+//                arr[i][0] = Integer.parseInt(st.nextToken());
+//                arr[i][1] = Integer.parseInt(st.nextToken());
+//            }
+//
+//            arr = Arrays.stream(arr)
+//                    .sorted((o1, o2) -> o1[0] - o2[0])
+//                    .toArray(int[][]::new);
+//
+//            int score = arr[0][1];
+//            int cnt = 1;
+//            for (int i = 1; i < N; i++) {
+//                if (score >= arr[i][1]) {
+//                    score = arr[i][1];
+//                    cnt++;
+//                }
+//            }
+//
+//            sb.append(String.valueOf(cnt) + "\n");
+//        }
+
+        // 발전된 버전
         while (T-- > 0) {
-            N = Integer.parseInt(br.readLine());
-            arr = new int[N][2];
+            int N = Integer.parseInt(br.readLine());
+            int[] interviews = new int[N];
+
             for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                arr[i][0] = Integer.parseInt(st.nextToken());
-                arr[i][1] = Integer.parseInt(st.nextToken());
+                String[] scores = br.readLine().split(" ");
+                int document = Integer.parseInt(scores[0]) - 1;
+                int interview = Integer.parseInt(scores[1]) - 1;
+                interviews[document] = interview;
             }
 
-            arr = Arrays.stream(arr)
-                    .sorted((o1, o2) -> o1[0] - o2[0])
-                    .toArray(int[][]::new);
-
-            int score = arr[0][1];
+            int score = interviews[0];
             int cnt = 1;
             for (int i = 1; i < N; i++) {
-                if (score >= arr[i][1]) {
-                    score = arr[i][1];
+                if (score >= interviews[i]) {
+                    score = interviews[i];
                     cnt++;
                 }
             }
 
-            sb.append(String.valueOf(cnt) + "\n");
+            sb.append(cnt).append('\n');
         }
         bw.write(sb.toString());
         bw.close();
