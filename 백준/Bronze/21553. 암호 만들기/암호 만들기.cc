300d921ab7e718a47f201711c2ec83c87474fb56
Now <156046839+Kguswo@users.noreply.github.com>
2024-11-30 23:54:27 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,20 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string A, P;
+    cin >> A >> P;
+
+    cout << P << "\n";
+
+    return 0;
+}
