3592f1d6c3fef805b9290d471e99b3e4a34ba7d6
Now <156046839+Kguswo@users.noreply.github.com>
2024-12-11 04:53:26 +0900

    [Gold IV] Title: 전화번호 목록, Time: 328 ms, Memory: 107912 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,57 +1,83 @@
 /**
  * Author: nowalex322, Kim HyeonJae
  */
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+	class Trie{
+		Trie[] children = new Trie[10];
+		boolean isEnd;
+		Trie(){
+			for(int i=0; i<10; i++) {
+				children[i] = null;
+			}
+			isEnd = false;
+		}
+	}
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static Trie root;
+	static boolean flag;
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
 
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
+	public void solution() throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+//		br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public void solution() throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
-//        br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_5052_전화번호목록/input.txt")));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		while(T-->0) {
+			root = new Trie();
+			int n = Integer.parseInt(br.readLine());
+			
+			flag = true;
+			for(int i=0; i<n; i++) {
+				String num = br.readLine();
+				insertNum(num);
+			}
+			
+			sb.append(flag ? "YES" : "NO").append("\n");
+		}
 
-        int T = Integer.parseInt(br.readLine());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            String[] s = new String[n];
-            for (int i = 0; i < n; i++) {
-                String num = br.readLine();
-                s[i] = num;
-            }
-
-            Arrays.sort(s);
-
-            boolean flag = true;
-            for (int i = 0; i < s.length - 1; i++) {
-                if (s[i + 1].startsWith(s[i])) {
-                    flag = false;
-                    sb.append("NO").append("\n");
-                    break;
-                }
-            }
-            if (flag) {
-                sb.append("YES").append("\n");
-            }
-        }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	private void insertNum(String num) {
+		Trie trie = root;
+		
+		for(int i=0; i<num.length(); i++) {
+			int number = num.charAt(i) - '0';
+			
+			if(trie.isEnd) {
+				flag = false;
+				return;
+			}
+			
+			if(trie.children[number]==null) {
+				trie.children[number] = new Trie();
+			}
+			
+			trie = trie.children[number];
+		}
+		
+		if(trie.isEnd) {
+			flag = false;
+			return;
+		}
+		
+		for(int i=0; i<10; i++) {
+			if(trie.children[i] != null) {
+				flag = false;
+				return;
+			}
+		}
+		trie.isEnd = true;
+	}
 }
