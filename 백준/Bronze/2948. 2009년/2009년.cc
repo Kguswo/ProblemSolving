7935fbadd8710ee0a204e7da9a5c57a54e7e1394
Now <156046839+Kguswo@users.noreply.github.com>
2024-12-12 00:06:59 +0900

    Update 2009년.cc

--- 백준/Bronze/2948. 2009년/2009년.cc
+++ 백준/Bronze/2948. 2009년/2009년.cc
@@ -1,27 +1,42 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
-int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-char w[7][20] = {"Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"};
+// #define int long long
+#define MOD 1000000007
+#define INF LLONG_MAX
+#define ALL(v) v.begin(), v.end()
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+string Day[] = {"Wednesday", "Thursday", "Friday", "Saturday",
+                "Sunday",    "Monday",   "Tuesday"};
 
 void solve() {
     // 2008.12.31은 수요일
-    int d, m;
-    cin >> d >> m;
-    
-    int total_days = d;  // Start with the input day
-    for (int i = 1; i < m; i++) {
-        total_days += month[i];  // Add days from previous months
+    int D, M;
+    cin >> D >> M;
+    int res = D;
+    for (int i = 1; i < M; i++) {
+        res += month[i];
     }
-    
-    cout << w[total_days % 7] << '\n';
+    cout << Day[res % 7] << "\n";
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tt = 1;
+    int tt = 1;  // 기본적으로 1번의 테스트 케이스를 처리
+    // cin >> tt;    // 테스트 케이스 수 입력 (필요 시)
+
     while (tt--) {
         solve();
     }
