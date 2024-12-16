5ac6e2dea3aa37a931f994be90528e6d435b95ac
Now <156046839+Kguswo@users.noreply.github.com>
2024-12-16 16:31:28 +0900

    [Gold V] Title: 내려가기, Time: 376 ms, Memory: 52404 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,31 +1,55 @@
-import java.util.Scanner;
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static int board[][], maxdp[][], mindp[][];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		board = new int [N][3];
-		maxdp = new int [N+1][3];
-		mindp = new int [N+1][3];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<3; j++) {
-				board[i][j] = sc.nextInt();
-			}
-		}
-		for(int i=1; i<=N; i++) {
-			for(int j=0; j<3; j++) {
-				maxdp[i][0] = board[i-1][0] + Math.max(maxdp[i-1][0], maxdp[i-1][1]);
-				maxdp[i][1] = board[i-1][1] + Math.max(maxdp[i-1][0], Math.max(maxdp[i-1][1], maxdp[i-1][2]));
-				maxdp[i][2] = board[i-1][2] + Math.max(maxdp[i-1][1], maxdp[i-1][2]);
-				mindp[i][0] = board[i-1][0] + Math.min(mindp[i-1][0], mindp[i-1][1]);
-				mindp[i][1] = board[i-1][1] + Math.min(mindp[i-1][0], Math.min(mindp[i-1][1], mindp[i-1][2]));
-				mindp[i][2] = board[i-1][2] + Math.min(mindp[i-1][1], mindp[i-1][2]);
-			}
-		}
-		System.out.print(Math.max(maxdp[N][0], Math.max(maxdp[N][1], maxdp[N][2])) + " ");
-		System.out.println(Math.min(mindp[N][0], Math.min(mindp[N][1], mindp[N][2])));
-	}
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    static int N, board[][], maxdp[][], mindp[][];
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    public void solution() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+//        br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_2096_내려가기/input.txt")));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        maxdp = new int[N + 1][3];
+        mindp = new int[N + 1][3];
+        maxdp[1][0] = mindp[1][0] = board[0][0];
+        maxdp[1][1] = mindp[1][1] = board[0][1];
+        maxdp[1][2] = mindp[1][2] = board[0][2];
+
+        for (int i = 2; i <= N; i++) {
+            maxdp[i][0] = board[i - 1][0] + Math.max(maxdp[i - 1][0], maxdp[i - 1][1]);
+            mindp[i][0] = board[i - 1][0] + Math.min(mindp[i - 1][0], mindp[i - 1][1]);
+            maxdp[i][1] = board[i - 1][1] + Math.max(maxdp[i - 1][0], Math.max(maxdp[i - 1][1], maxdp[i - 1][2]));
+            mindp[i][1] = board[i - 1][1] + Math.min(mindp[i - 1][0], Math.min(mindp[i - 1][1], mindp[i - 1][2]));
+            maxdp[i][2] = board[i - 1][2] + Math.max(maxdp[i - 1][1], maxdp[i - 1][2]);
+            mindp[i][2] = board[i - 1][2] + Math.min(mindp[i - 1][1], mindp[i - 1][2]);
+        }
+
+        int maxRes = Math.max(maxdp[N][0], Math.max(maxdp[N][1], maxdp[N][2]));
+        int minRes = Math.min(mindp[N][0], Math.min(mindp[N][1], mindp[N][2]));
+        StringBuilder sb = new StringBuilder();
+        sb.append(maxRes).append(" ").append(minRes);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
