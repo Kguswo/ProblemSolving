2535aeca926e52c6138bc6c3153c181f585fe445
Now <156046839+Kguswo@users.noreply.github.com>
2024-12-16 14:56:15 +0900

    [Medium] Title: 433-minimum-genetic-mutation, Time: 0 ms (100%), Space: 41.8 MB (23.85%) - LeetHub

---
+++ LeetCode/Medium/0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        int start = hashing(startGene);
+        int end = hashing(endGene);
+        List<Integer> geneBank = new ArrayList<>();
+        for(String s : bank){
+            geneBank.add(hashing(s));
+        }
+
+        if(!geneBank.contains(end)) return -1;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> visited = new LinkedList<>();
+
+        queue.offer(start);
+        visited.add(start);
+
+        int cnt = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int s = 0; s < size; s++){
+                int curr = queue.poll();
+                if(curr == end) return cnt;
+
+                for(int idx = 0; idx < 8; idx++){
+                    int idxNum = curr/ (int) Math.pow(10, idx) % 10;
+                    for(int newNum= 1; newNum <= 4; newNum++){
+                         if(idxNum != newNum) {
+                            int next = curr + (newNum - idxNum) * (int) Math.pow(10, idx);
+                            if(geneBank.contains(next) && !visited.contains(next)){
+                                queue.offer(next);
+                                visited.add(next);
+                            }
+                         }
+                    }
+                }
+            }
+            cnt++;
+        }
+        return -1;
+    }
+    
+    private int hashing(String str){ // 10진수 해싱 
+        int num = 0;
+        for(int i=0; i<8; i++){
+            num *= 10;
+            char c = str.charAt(i);
+            if(c=='A') num += 1;
+            else if(c=='C') num += 2;
+            else if(c=='G') num += 3;
+            else num += 4;
+        }
+        return num;
+    }
+}
