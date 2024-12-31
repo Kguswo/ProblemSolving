c7d342f1c5620b9bf29a8ec87177083e468425ad
Now <156046839+Kguswo@users.noreply.github.com>
2024-12-31 21:15:04 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,14 @@
-SELECT ai.NAME, ai.DATETIME
-    FROM ANIMAL_INS AS ai
-        LEFT JOIN ANIMAL_OUTS AS ao ON ai.ANIMAL_ID = ao.ANIMAL_ID
-            WHERE ao.ANIMAL_ID IS NULL
-                ORDER BY ai.DATETIME ASC
-                    LIMIT 3;
+# SELECT ai.NAME, ai.DATETIME
+#     FROM ANIMAL_INS AS ai
+#         LEFT JOIN ANIMAL_OUTS AS ao ON ai.ANIMAL_ID = ao.ANIMAL_ID
+#             WHERE ao.ANIMAL_ID IS NULL
+#                 ORDER BY ai.DATETIME ASC
+#                     LIMIT 3;
+                    
+SELECT i.NAME, i.DATETIME
+    FROM ANIMAL_INS AS i
+        LEFT JOIN ANIMAL_OUTS AS o
+            ON i.ANIMAL_ID = o.ANIMAL_ID
+                WHERE o.ANIMAL_ID IS NULL
+                    ORDER BY i.DATETIME ASC
+                        LIMIT 3;
