5e81b5aa00aa29a52d569beb202a8275a8a145c2
Now <156046839+Kguswo@users.noreply.github.com>
2025-01-05 22:53:40 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,47 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long
+#define MOD 1000000007
+#define INF LLONG_MAX
+#define ALL(v) v.begin(), v.end()
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+void solve() {
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    vector<vector<int>> dp(str1.length() + 1,
+                           vector<int>(str2.length() + 1, 0));
+
+    for (int i = 1; i <= str1.length(); i++) {
+        for (int j = 1; j <= str2.length(); j++) {
+            if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    cout << dp[str1.length()][str2.length()] << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;  // 기본적으로 1번의 테스트 케이스를 처리
+    // cin >> tt;    // 테스트 케이스 수 입력 (필요 시)
+
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
