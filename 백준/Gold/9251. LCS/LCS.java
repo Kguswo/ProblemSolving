7dc9aaa790c3758c73a4864f78f3bad465f004e0
Now <156046839+Kguswo@users.noreply.github.com>
2025-01-05 22:48:11 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 18544 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,25 +1,41 @@
-import java.util.Scanner;
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static String str1, str2;
-	static int[][] dp;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
 
-		str1 = sc.next();
-		str2 = sc.next();
-		dp = new int [str1.length()+1][str2.length()+1];
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
+
+	public void solution() throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+//		br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		
+		int[][] dp = new int[str1.length() + 1][str2.length() + 1];
 		for(int i=1; i<=str1.length(); i++) {
 			for(int j=1; j<=str2.length(); j++) {
-				
-				if(str1.charAt(i-1) == str2.charAt(j-1)) {
-					dp[i][j] = dp[i-1][j-1] + 1;
-				}
-				else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-				}
+				if(str1.charAt(i-1)==str2.charAt(j-1)) dp[i][j] = Math.max(dp[i-1][j-1] + 1, dp[i][j]);
+				else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
 			}
 		}
-		System.out.println(dp[str1.length()][str2.length()]);
+//		for(int i=0; i<=str1.length(); i++) {
+//			for(int j=0; j<=str2.length(); j++) {
+//				System.out.print(dp[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+		bw.write(String.valueOf(dp[str1.length()][str2.length()]));
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
