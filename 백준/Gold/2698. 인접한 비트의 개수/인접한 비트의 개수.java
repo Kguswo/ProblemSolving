340f875427845005794ff98eea4244292d7600d6
Now <156046839+Kguswo@users.noreply.github.com>
2025-01-10 18:31:51 +0900

    [Gold IV] Title: 인접한 비트의 개수, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.java
+++ 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.java
@@ -10,7 +10,7 @@ public class Main {
     static BufferedWriter bw;
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    static int TC, N, K, dp[][][];
+    static int TC, N, K, dp[][];
 
     public static void main(String[] args) throws Exception {
         new Main().solution();
@@ -22,28 +22,21 @@ public class Main {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         TC = Integer.parseInt(br.readLine());
-        while (TC-- > 0) {
+        dp = new int[101][101];
+        dp[0][0] = 1;
+        dp[1][0] = 2;
+        for(int i=2;i<=100;i++){
+            for(int j=i-1;j>0;j--){
+                dp[i][j] = dp[i-1][j] + dp[i-2][j] + (dp[i-1][j-1] - dp[i-2][j-1]);
+            }
+            dp[i][0] = dp[i-1][0] + dp[i-2][0];
+        }
+        
+        while(TC-->0){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            dp = new int[N + 1][K + 1][2];
-            dp[1][0][0] = 1;
-            dp[1][0][1] = 1;
-
-            for (int i = 2; i <= N; i++) {
-                for (int j = 0; j <= Math.min(i - 1, K); j++) {
-                    for (int k = 0; k < 2; k++) {
-                        dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
-
-                        if (j == 0) { // 이거 없으면 -1 인덱스 접근가능 오류
-                            dp[i][j][1] = dp[i - 1][j][0];
-                        } else {
-                            dp[i][j][1] = dp[i - 1][j][0] + dp[i - 1][j - 1][1];
-                        }
-                    }
-                }
-            }
-            sb.append(dp[N][K][0] + dp[N][K][1]).append('\n');
+            sb.append(String.valueOf(dp[N][K])).append('\n');
         }
         bw.write(sb.toString());
         bw.flush();
