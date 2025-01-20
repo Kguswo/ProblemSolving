1f96e66a89c86940cb164c8681976f66a8da2a02
Now <156046839+Kguswo@users.noreply.github.com>
2025-01-20 21:38:14 +0900

    [Gold II] Title: 컵라면, Time: 708 ms, Memory: 71064 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -48,11 +48,22 @@ public class Main {
 
         while(!pq.isEmpty()) {
             Work currW = pq.poll();
+            /*
+            데드라인까지 개수 부족하게는 넣을 수 있음 (데드라인 1 3 5 세개면 다 가능)
+            근데 데드라인 오버되도록 진행하면 문제가 발생
+            예를들어
+            1 4
+            2 5
+            3 6
+            3 7
+            이런경우에는 최적해가 5 6 7 로 진행해야함../ 데드라인이 최대 그날까진 해야한다이므로 그 전에 더 높은거 해도됨
+            그러므로 이런 경우에는 고른 라면중에 제일 적게 받은과제를 빼고 이번에 고른걸 선택
+             */
             if(ramens.size() < currW.deadline) {
                 ramens.add(currW.ramen);
                 maxRamen += currW.ramen;
             }
-            else if(!ramens.isEmpty() && ramens.peek() < currW.ramen) { // 꽉 찼는데 최대컵라면만 갱신
+            else if(!ramens.isEmpty() && ramens.peek() < currW.ramen) { // 최저 컵라면 교체
                 maxRamen -= ramens.poll();
                 ramens.add(currW.ramen);
                 maxRamen += currW.ramen;
