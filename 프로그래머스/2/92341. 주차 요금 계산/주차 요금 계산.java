b401d2ff7ef1d2856de4563867ea8ab7bbbc83e2
Now <156046839+Kguswo@users.noreply.github.com>
2025-01-20 23:56:33 +0900

    [level 2] Title: 주차 요금 계산, Time: 9.06 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -0,0 +1,59 @@
+import java.util.*;
+/*
+  map으로 관리 
+  Integer, ArrayList<Integer> 로 입출차관리 -> 길이 홀수면 23:59로채움
+  Integer, Integer로 합 관리
+*/
+class Solution {
+    static int cTime, cNumber;
+    static String cActivity;
+    static Map<Integer, Integer> timeSumMap = new HashMap<>();
+    static Map<Integer, ArrayList<Integer>> timeTableMap = new HashMap<>();
+    static List<Integer> carNums;
+    static int[] res;
+    public int[] solution(int[] fees, String[] records) {
+        for(String s : records){
+            // s = s.subString(1, s.length()-1);
+            String[] split = s.split(" ");
+            
+            String[] cTimeSplit = split[0].split(":");
+            cTime = Integer.parseInt(cTimeSplit[0]) * 60 + Integer.parseInt(cTimeSplit[1]);
+            cNumber = Integer.parseInt(split[1]);
+            cActivity = split[2];
+            
+            if(cActivity.equals("IN")){
+                timeTableMap.putIfAbsent(cNumber, new ArrayList<Integer>());
+                timeTableMap.get(cNumber).add(cTime); //입차시간
+            }
+            else{
+                ArrayList<Integer> timeList = timeTableMap.get(cNumber);
+                
+                int timeGap = cTime - timeList.get(timeList.size()-1); // 출차 - 마지막입차
+                int sum = timeSumMap.getOrDefault(cNumber, 0) + timeGap;
+                timeSumMap.put(cNumber, sum);
+                timeList.add(cTime); // 출차시
+            }
+        }
+        
+        for(int num : timeTableMap.keySet()){
+            ArrayList<Integer> timeList = timeTableMap.get(num);
+            if(timeList.size() % 2 == 1){
+                int timeGap = 23 * 60 + 59 - timeList.get(timeList.size()-1);
+                int sum = timeSumMap.getOrDefault(num, 0) + timeGap;
+                timeSumMap.put(num, sum);
+            }
+        }
+        
+        carNums = new ArrayList<Integer>(timeSumMap.keySet());
+        Collections.sort(carNums);
+        res = new int[carNums.size()];
+        for(int i=0; i<carNums.size(); i++){
+            int money = fees[1];
+            if(timeSumMap.get(carNums.get(i)) > fees[0]){
+                money += (int) Math.ceil((double) (timeSumMap.get(carNums.get(i))-fees[0]) / fees[2]) * fees[3];
+            }
+            res[i] = money;
+        }
+        return res;
+    }
+}
