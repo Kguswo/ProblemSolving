f4c1c7e939e4c71c7029e199a6b235767a0e427a
Now <156046839+Kguswo@users.noreply.github.com>
2025-02-05 21:07:11 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1068 ms, Memory: 179796 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -5,75 +5,78 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    static int N, M;
-    static int[][] board;
-    static boolean[][][] visited; // 벽을 부수었는지의 여부를 추가
-    static int[][][] length; // 벽을 부수었을 때와 부수지 않았을 때의 길이를 저장
-    static int mincount = Integer.MAX_VALUE;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
+	static int N, M;
+	static int[][] board;
+	static boolean[][][] visited;
+	static int[][][] length;
+	static int mincount = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(new File("input.txt"));
 
-        N = sc.nextInt();
-        M = sc.nextInt();
-        board = new int[N + 1][M + 1];
-        visited = new boolean[N + 1][M + 1][2]; // 벽을 부수었는지의 여부를 추가
-        length = new int[N + 1][M + 1][2]; // 벽을 부수었을 때와 부수지 않았을 때의 길이를 저장
+		N = sc.nextInt();
+		M = sc.nextInt();
+		board = new int[N + 2][M + 2];
+		visited = new boolean[N + 2][M + 2][2];
+		length = new int[N + 2][M + 2][2];
 
-        for (int r = 1; r <= N; r++) {
-            String str = sc.next();
-            for (int c = 1; c <= M; c++) {
-                board[r][c] = str.charAt(c - 1) - '0';
-            }
-        }
+		for (int r = 0; r < N + 2; r++) {
+			for (int c = 0; c < M + 2; c++) {
+				board[r][c] = 1;
+			}
+		}
+		for (int r = 1; r <= N; r++) {
+			String str = sc.next();
+			for (int c = 1; c <= M; c++) {
+				board[r][c] = str.charAt(c - 1) - '0';
+			}
+		}
 
-        bfs(1, 1);
+		bfs(1, 1);
+
         if (mincount == Integer.MAX_VALUE) {
             System.out.println(-1);
         } else {
             System.out.println(mincount);
         }
-    }
+	}
 
-    static void bfs(int r, int c) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{r, c, 0}); // 벽을 부수지 않았을 때의 정보 추가
+	static void bfs(int r, int c) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] { r, c, 1 });
 
-        visited[r][c][0] = true; // 벽을 부수지 않았음을 표시
-        length[r][c][0] = 1;
+		visited[r][c][1] = true;
+		length[r][c][1]= 1;
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currentr = current[0];
-            int currentc = current[1];
-            int currentwallbreak = current[2];
+		while (queue.isEmpty() == false) {
+			int[] current = queue.poll();
 
-            // 도착점에 도달하면 최단 경로 업데이트
+			int currentr = current[0];
+			int currentc = current[1];
+			int currentwallbreak = current[2];
+			
+			//최소길이 갱신
             if (currentr == N && currentc == M) {
-                mincount = Math.min(mincount, length[currentr][currentc][currentwallbreak]);
+                mincount = Math.min(length[currentr][currentc][currentwallbreak], mincount);
             }
 
-            for (int idx = 0; idx < 4; idx++) {
-                int newr = currentr + dr[idx];
-                int newc = currentc + dc[idx];
-
-                if (newr >= 1 && newr <= N && newc >= 1 && newc <= M) {
-                    // 이동할 곳이 빈 공간이고 아직 방문하지 않았을 때
-                    if (board[newr][newc] == 0 && !visited[newr][newc][currentwallbreak]) {
+			for (int idx = 0; idx <= 3; idx++) {
+				int newr = currentr + dr[idx];
+				int newc = currentc + dc[idx];
+                    if (board[newr][newc] == 0 && visited[newr][newc][currentwallbreak] == false) {
                         queue.add(new int[]{newr, newc, currentwallbreak});
                         visited[newr][newc][currentwallbreak] = true;
                         length[newr][newc][currentwallbreak] = length[currentr][currentc][currentwallbreak] + 1;
                     }
-                    // 이동할 곳이 벽이고 아직 벽을 부수지 않았을 때
-                    else if (board[newr][newc] == 1 && currentwallbreak == 0 && !visited[newr][newc][1]) {
-                        queue.add(new int[]{newr, newc, 1});
-                        visited[newr][newc][1] = true;
-                        length[newr][newc][1] = length[currentr][currentc][currentwallbreak] + 1;
+                    else if (newr >= 1 && newr <= N && newc >= 1 && newc <= M && board[newr][newc] == 1 && visited[newr][newc][0]== false && currentwallbreak == 1) {
+                        queue.add(new int[]{newr, newc, 0});
+                        visited[newr][newc][0] = true;
+                        length[newr][newc][0] = length[currentr][currentc][currentwallbreak] + 1;
                     }
-                }
-            }
-        }
-    }
+			}
+		}
+	}
 }
