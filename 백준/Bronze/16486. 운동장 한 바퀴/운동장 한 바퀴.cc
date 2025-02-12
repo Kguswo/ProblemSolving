40272166e83f618de7eec54ddf85f9e041e30976
Now <156046839+Kguswo@users.noreply.github.com>
2025-02-12 23:56:10 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,40 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long
+#define MOD 1000000007
+#define INF LLONG_MAX
+#define ALL(v) v.begin(), v.end()
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+void solve() {
+    double d1, d2;
+    cin >> d1 >> d2;
+
+    const double PI = 3.141592;
+
+    double res = 2 * d1 + PI * 2 * d2;
+
+    cout << fixed << setprecision(6) << res << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;  // 기본적으로 1번의 테스트 케이스를 처리
+    // cin >> tt;    // 테스트 케이스 수 입력 (필요 시)
+
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
