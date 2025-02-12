5c448388e88861de619cb748a51f502a11674f2a
Now <156046839+Kguswo@users.noreply.github.com>
2025-02-12 23:40:04 +0900

    [Bronze III] Title: 대지, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.cc
@@ -0,0 +1,52 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long
+#define MOD 1000000007
+#define INF LLONG_MAX
+#define ALL(v) v.begin(), v.end()
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> x(n), y(n);
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    if (n == 1) {
+        cout << 0 << "\n";
+        return;
+    }
+
+    int min_x = *min_element(x.begin(), x.end());
+    int max_x = *max_element(x.begin(), x.end());
+    int min_y = *min_element(y.begin(), y.end());
+    int max_y = *max_element(y.begin(), y.end());
+
+    int area = (max_x - min_x) * (max_y - min_y);
+    cout << area << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;  // 기본적으로 1번의 테스트 케이스를 처리
+    // cin >> tt;    // 테스트 케이스 수 입력 (필요 시)
+
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
