131fbceacaa1d3caaa1996a2608fe22f3205b4ba
Now <156046839+Kguswo@users.noreply.github.com>
2025-02-12 23:47:03 +0900

    [Bronze II] Title: TV 크기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/1297. TV 크기/TV 크기.cc
@@ -0,0 +1,42 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define int long long
+#define MOD 1000000007
+#define INF LLONG_MAX
+#define ALL(v) v.begin(), v.end()
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+void solve() {
+    double D;
+    int H, W;
+    cin >> D >> H >> W;
+
+    double x = sqrt((D * D) / (H * H + W * W));
+
+    int height = (int)(H * x);
+    int width = (int)(W * x);
+
+    cout << height << " " << width << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;  // 기본적으로 1번의 테스트 케이스를 처리
+    // cin >> tt;    // 테스트 케이스 수 입력 (필요 시)
+
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
