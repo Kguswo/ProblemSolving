c091292c36377baa84a352334d0290004871f071
Now <156046839+Kguswo@users.noreply.github.com>
2025-02-19 02:30:42 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 196 ms, Memory: 18468 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
@@ -6,65 +6,95 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static StringTokenizer st;
-    static int N, M, dp[][][], res = Integer.MAX_VALUE;
+    static class FastReader {
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[16384];
+            bufferPointer = bytesRead = 0;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, buffer.length);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            return neg ? -ret : ret;
+        }
+    }
+    static FastReader fr = new FastReader();
+    static int N, M, prev[][], curr[][]  , res = Integer.MAX_VALUE;
     public static void main(String[] args) throws Exception {
         new Main().solution();
     }
 
     public void solution() throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        //br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_17485_진우의달여행Large/input.txt")));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = fr.nextInt();
+        M = fr.nextInt();
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        dp = new int[N+1][M+1][3];
-
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= M; j++) {
-                for(int k=0; k < 3; k++) {
-                    dp[i][j][k] = Integer.MAX_VALUE;
-                }
-            }
+        prev = new int[M + 1][3];
+        curr = new int[M + 1][3];
+        for(int j = 0; j <= M; j++) {
+            Arrays.fill(prev[j], Integer.MAX_VALUE);
+            Arrays.fill(curr[j], Integer.MAX_VALUE);
         }
-
 
         for(int i=1; i<=N; i++) {
             if(i==1){
-                st = new StringTokenizer(br.readLine());
-                for(int j=1; j<=M; j++){
-                    int num = Integer.parseInt(st.nextToken());
-                    for(int k=0; k<3; k++) {
-                        dp[i][j][k] = num;
+                for (int j = 1; j <= M; j++) {
+                    int num = fr.nextInt();
+                    for (int k = 0; k < 3; k++) {
+                        prev[j][k] = num;
                     }
                 }
             }
             else{
-                st = new StringTokenizer(br.readLine());
-                for(int j=1; j<=M; j++) {
-                    int num = Integer.parseInt(st.nextToken());
-                    // 밑으로 |
-                    dp[i][j][0] = Math.min(dp[i-1][j][1], dp[i-1][j][2]) + num;
-                    // 왼오\방향
-                    if(j>=2) dp[i][j][1] = Math.min(dp[i-1][j-1][0], dp[i-1][j-1][2]) + num;
-                    // 오왼/ 방향
-                    if(j<M) dp[i][j][2] = Math.min(dp[i-1][j+1][0], dp[i-1][j+1][1]) + num;
+                for (int j = 1; j <= M; j++) {
+                    int num = fr.nextInt();
+
+                    int down = Math.min(prev[j][1], prev[j][2]);
+                    int left = (j > 1) ? Math.min(prev[j - 1][0], prev[j - 1][2]) : Integer.MAX_VALUE;
+                    int right = (j < M) ? Math.min(prev[j + 1][0], prev[j + 1][1]) : Integer.MAX_VALUE;
+
+                    if (down != Integer.MAX_VALUE) curr[j][0] = down + num; // 밑으로 |
+                    if (left != Integer.MAX_VALUE) curr[j][1] = left + num; // 왼오\방향
+                    if (right != Integer.MAX_VALUE) curr[j][2] = right + num; // 오왼/ 방향
+                }
+                int[][] temp = prev;
+                prev = curr;
+                curr = temp;
+                for(int j = 0; j <= M; j++) {
+                    Arrays.fill(curr[j], Integer.MAX_VALUE);
                 }
             }
         }
 
         for(int j=1; j<=M; j++) {
             for(int k=0; k<3; k++) {
-                res = Math.min(res, dp[N][j][k]);
+                res = Math.min(res, prev[j][k]);
             }
         }
         System.out.println(res);
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
