313277cb47c125a48a58cdfe5f3b6deb52e034c0
Now <156046839+Kguswo@users.noreply.github.com>
2025-03-04 13:29:20 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 604 ms, Memory: 59312 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -1,104 +1,99 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static BufferedReader br;
-	static BufferedWriter bw;
-	static StringTokenizer st;
-	static int N, M;
-	static int[] minTree, maxTree, num;
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M, h, size;
+    static int[] nums, maxTree, minTree;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    public void solution() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-//		br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
-		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_2357_최댓값과최솟값/input.txt")));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		int treeHeight = (int) Math.ceil(Math.log(N) / Math.log(2));
-		int treeSize = (int) Math.pow(2, treeHeight + 1) - 1;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        /*
+        높이 : log_2(N) + 1
+        배열크기 : 2^h - 1
+         */
 
-		minTree = new int[treeSize];
-		maxTree = new int[treeSize];
-		num = new int[N+1];
+        h = (int) Math.ceil(Math.log(N) / Math.log(2)) + 1;
+        size = (int) Math.pow(2, h) - 1;
 
-		for (int i = 1; i <= N; i++) {
-			num[i] = Integer.parseInt(br.readLine());
-		}
-//		System.out.println(Arrays.toString(num));
+        maxTree = new int[size];
+        minTree = new int[size];
 
-		initMinTree(1, N, 1);
-		initMaxTree(1, N, 1);
+        initMaxTree(1, 1, N);
+        initMinTree(1, 1, N);
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			bw.write(getMin(1, N, 1, from, to) + " " + getMax(1, N, 1, from, to) + "\n");
-		}
-		
+        int s, e;
+        for (int q = 0; q < M; q++) {
+            st = new StringTokenizer(br.readLine());
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            sb.append(getMin(1, 1, N, s, e)).append(" ").append(getMax(1, 1, N, s, e)).append("\n");
+        }
+        System.out.println(sb);
         bw.flush();
         bw.close();
         br.close();
+    }
 
-	}
+    private int initMinTree(int node, int start, int end) {
+        if (start == end) return minTree[node] = nums[start];
 
-	/*
-	 * 세그먼트 트리 초기화 
-	 * node : 세그먼트 트리의 정점 번호 
-	 * start : 이 정점이 관리하는 연속 구간의 왼쪽 끝 
-	 * end : 이 정점이 관리하는 연속 구간의 오른쪽 끝
-	 * 
-	 */
-	private static int initMinTree(int start, int end, int node) { // 최솟값 트리
+        int mid = start + (end - start) / 2;
 
-		if (start == end) return minTree[node] = num[start];
+        return minTree[node] = Math.min(initMinTree(node * 2, start, mid), initMinTree(node * 2 + 1, mid + 1, end));
+    }
 
-		int mid = (start + end) / 2;
+    private int initMaxTree(int node, int start, int end) {
+        if (start == end) return maxTree[node] = nums[start];
 
-		return minTree[node] = Math.min(initMinTree(start, mid, node*2), initMinTree(mid + 1, end, node*2 + 1));
+        int mid = start + (end - start) / 2;
 
-	}
-	
-	private static int initMaxTree(int start, int end, int node) { // 최댓값 트리
-		
-		if (start == end) return maxTree[node] = num[start];
-		
-		int mid = (start + end) / 2;
-		
-		return maxTree[node] = Math.max(initMaxTree(start, mid, node*2), initMaxTree(mid + 1, end, node*2 + 1));
-		
-	}
+        return maxTree[node] = Math.max(initMaxTree(node * 2, start, mid), initMaxTree(node * 2 + 1, mid + 1, end));
+    }
 
-	// from ~ to 구간 최솟값
-	private static int getMin(int start, int end, int node, int from, int to) {
-		// 보고있는 노드의 구간이 원하는 범위 밖인 경우
-		if (from > end || to < start) return Integer.MAX_VALUE;
+    private int getMax(int node, int start, int end, int from, int to) {
+        /*
+            찾고자 하는 구간 벗어날땐 리턴
+            start~end에 속할땐 그 구간 값 반환
+            아래로 계속쪼개기
+         */
+        if (end < from || to < start) return Integer.MIN_VALUE;
 
-		// 보고있는 노드의 구간이 원하는 범위 안인 경우
-		if (from <= start && to >= end) return minTree[node];
+        if (from <= start && end <= to) return maxTree[node];
 
-		// 그 외 걸치는 경우는 재귀적으로 두 자식노드로 나눠서 최솟값 구하기
-		int mid = (start + end) / 2;
-		
-		return Math.min(getMin(start, mid, node*2, from, to), getMin(mid + 1, end, node*2 + 1, from, to));
-	
-	}
-	
-	// from ~ to 구간 최댓값
-	private static int getMax(int start, int end, int node, int from, int to) {
-		// 보고있는 노드의 구간이 원하는 범위 밖인 경우
-		if (from > end || to < start) return Integer.MIN_VALUE;
-		
-		// 보고있는 노드의 구간이 원하는 범위 안인 경우
-		if (from <= start && to >= end) return maxTree[node];
-		
-		// 그 외 걸치는 경우는 재귀적으로 두 자식노드로 나눠서 최댓값 구하기
-		int mid = (start + end) / 2;
-		
-		return Math.max(getMax(start, mid, node*2, from, to), getMax(mid + 1, end, node*2 + 1, from, to));
-		
-	}
+        int mid = start + (end - start) / 2;
 
+        return Math.max(getMax(node * 2, start, mid, from, to), getMax(node * 2 + 1, mid + 1, end, from, to));
+    }
+
+    private int getMin(int node, int start, int end, int from, int to) {
+        if (end < from || to < start) return Integer.MAX_VALUE;
+
+        if (from <= start && end <= to) return minTree[node];
+
+        int mid = start + (end - start) / 2;
+
+        return Math.min(getMin(node * 2, start, mid, from, to), getMin(node * 2 + 1, mid + 1, end, from, to));
+    }
 }
