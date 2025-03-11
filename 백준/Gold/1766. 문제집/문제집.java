dc3078d6207358dcbc5fb28ca9088ea172e98158
Now <156046839+Kguswo@users.noreply.github.com>
2025-03-11 13:48:53 +0900

    [Gold II] Title: 문제집, Time: 484 ms, Memory: 47488 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,55 +1,68 @@
 /**
- * Author : nowalex322, Kim Hyeonjae
+ * Author: nowalex322, Kim HyeonJae
  */
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static BufferedReader br;
-	static StringTokenizer st;
-	static StringBuilder sb;
-	static int N, M, degree[];
-	static List<Integer>[] list;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		list = new ArrayList[N+1];
-		degree = new int[N+1];
-		for(int i=1; i<=N; i++) {
-			list[i] = new ArrayList<Integer>();
-		}
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			list[A].add(B);
-			degree[B]++;
-		}
-		solve();
-		System.out.println(sb.toString());
-		
-	}
-	private static void solve() {
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-		for(int i=1; i<=N; i++) {
-			if(degree[i] == 0) pq.add(i);
-		}
-		
-		while(!pq.isEmpty()) {
-			int curr = pq.poll();
-			solveProblem(curr);
-			sb.append(curr + " ");
-			
-			for(int next : list[curr]) {
-				if(degree[next]==0) pq.add(next);
-			}
-		}
-	}
-	private static void solveProblem(int curr) {
-		for(int n : list[curr]) degree[n]--;
-	}
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M, degree[]; // degree : 선행문제수 진입차수배열
+    static ArrayList<Integer>[] probs;
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    public void solution() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+//        br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_1766_문제집/input.txt")));
+//        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 1-based;
+        degree = new int[N+1];
+        probs = new ArrayList[N+1];
+        for (int i = 1; i < N+1; i++) {
+            probs[i] = new ArrayList<>();
+        }
+
+        int A, B;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
+            // A문제는 B문제보다 먼저 풀어야함
+            probs[A].add(B);
+            degree[B]++;
+        }
+
+        // 풀 문제
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 1; i < N+1; i++) {
+            if(degree[i] == 0) pq.add(i);
+        }
+
+        while(!pq.isEmpty()) {
+            int currP = pq.poll();
+            sb.append(currP + " ");
+            for(int p : probs[currP]) {
+                degree[p]--;
+            }
+
+            degree[currP]--;
+            for(int nextP : probs[currP]) {
+                if(degree[nextP] == 0) pq.add(nextP);
+            }
+        }
+
+        System.out.println(sb.toString());
+        br.close();
+    }
 }
