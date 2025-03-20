b77f9ae471c591b63ae885fef4d9a52db866d9d1
Now <156046839+Kguswo@users.noreply.github.com>
2025-03-20 23:48:37 +0900

    [Bronze II] Title: 16진수, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,13 @@
+/**
+ * Author: nowalex322, Kim HyeonJae
+ */
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine(), 16);
+        System.out.println(n);
+    }
+}
