cc0eca9c8049d0a35783f1e934dca6709d000be1
Now <156046839+Kguswo@users.noreply.github.com>
2025-03-27 19:37:19 +0900

    [Silver V] Title: 줄세우기, Time: 160 ms, Memory: 15376 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -6,29 +6,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static StringTokenizer st;
+    public class FastReader {
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[16384];
+            bufferPointer = bytesRead = 0;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, buffer.length);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            return neg ? -ret : ret;
+        }
+    }
+    static FastReader fr;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
         new Main().solution();
     }
 
     public void solution() throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        //br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_10431_줄세우기/input.txt")));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int T = Integer.parseInt(br.readLine());
+        fr = new FastReader();
+        int T = fr.nextInt();
         while (T-- > 0) {
             List<Integer> height = new ArrayList<>();
-            st = new StringTokenizer(br.readLine());
-            int TC = Integer.parseInt(st.nextToken());
+            int TC = fr.nextInt();
             sb.append(TC).append(" ");
 
             int cnt = 0;
             for(int i = 1; i <= 20; i++) {
-                int h = Integer.parseInt(st.nextToken());
+                int h = fr.nextInt();
                 for(int j=height.size()-1; j>=0; j--) {
                     if(height.get(j) > h) cnt++;
                     else break;
@@ -40,6 +71,5 @@ public class Main {
             sb.append(cnt).append("\n");
         }
         System.out.println(sb.toString());
-        br.close();
     }
 }
