6a90e6c54484cdb8db67906b10572c0f488c8e0f
Now <156046839+Kguswo@users.noreply.github.com>
2025-04-25 06:03:21 +0900

    [Gold II] Title: 새로운 게임, Time: 84 ms, Memory: 12704 KB -BaekjoonHub

--- 백준/Gold/17780. 새로운 게임/새로운 게임.java
+++ 백준/Gold/17780. 새로운 게임/새로운 게임.java
@@ -1,13 +1,8 @@
-/**
- * Author: nowalex322, Kim HyeonJae
- */
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static BufferedReader br;
-//    static BufferedWriter bw;
     static StringTokenizer st;
     static int N, K;
     static int[] dr = {0, 0, -1, 1}, dc = {1, -1, 0, 0};
@@ -15,26 +10,22 @@ public class Main {
     static String[][] order;
     static StringBuilder sb;
     public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
-
-    public void solution() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-//        br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_17780_새로운게임/input.txt")));
-//        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        board = new int[N+2][N+2];
-        info = new int[K][3];
-        order = new String[N+2][N+2];
+        
+        board = new int[N+2][N+2]; // 색깔 저장
+        info = new int[K][3]; // 위치 및 방향 저장
+        order = new String[N+2][N+2]; // 각 칸마다 쌓여있는 말들의  
         for(int i=0; i<N+2; i++) {
             board[0][i] = 2;
             board[N+1][i] = 2;
             board[i][0] = 2;
             board[i][N+1] = 2;
         }
+        
         for(int i=1; i<N+1; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=1; j<N+1; j++) {
@@ -50,6 +41,7 @@ public class Main {
 
             order[info[i][0]][info[i][1]] = String.valueOf(i);
         }
+        //입력받기끝
 
         int cnt = 0;
         boolean flag = false;
@@ -60,6 +52,7 @@ public class Main {
                 int cr = info[i][0];
                 int cc = info[i][1];
 
+								// 맨 아래에 있는 말이 아니면 본인의지로 이동을 못하니까 패스
                 if(order[cr][cc] == null || Integer.valueOf(order[cr][cc].charAt(0) - '0') != i) continue;
 
                 int dir = info[i][2];
@@ -69,7 +62,9 @@ public class Main {
 
                 int color = board[nr][nc];
 
-                if(color == 0){ // white
+								// 흰색
+                if(color == 0){
+                
                     // next + curr(normal 순서)
                     String currOrder = order[cr][cc];
                     String nextOrder = order[nr][nc] == null ? "" : order[nr][nc];
@@ -77,18 +72,20 @@ public class Main {
                     order[cr][cc] = null;
                     order[nr][nc] = sb.append(nextOrder).append(currOrder).toString();
 
+										// 종료조건 체크								
                     if(order[nr][nc].length() >= 4) {
                         flag = true;
                         break;
                     }
 
-//                    info[i][0] = nr;
-//                    info[i][1] = nc;
                     // 말위치 이동 (전부)
                     updatePos(currOrder, nr, nc);
 
                 }
-                else if(color == 1){ // red
+                
+                // 빨강
+                else if(color == 1){
+                
                     // next + curr(reversed 순서)
                     String currOrder = order[cr][cc];
                     String nextOrder = order[nr][nc] == null ? "" : order[nr][nc];
@@ -105,8 +102,11 @@ public class Main {
 
                     updatePos(currOrder, nr, nc);
                 }
-                else if(color == 2){ // blue
-                    //info dir바꾸기
+                
+                // 파랑
+                else if(color == 2){
+                
+                    // 방향 반대로 바꾸기
                     int newDir = -1;
                     if(dir == 0 || dir == 1){
                         newDir = 1 - dir;
@@ -129,11 +129,11 @@ public class Main {
                     sb = new StringBuilder();
 
                     //반대칸 색깔체크
-                    if(board[oppositeR][oppositeC] == 0){ // white
+                    if(board[oppositeR][oppositeC] == 0){ // 반대편 흰색이면
                         order[cr][cc] = null;
                         order[oppositeR][oppositeC] = sb.append(nextOrder).append(currOrder).toString();
                     }
-                    else if(board[oppositeR][oppositeC] == 1){ // red
+                    else if(board[oppositeR][oppositeC] == 1){ // 반대편 빨간색이면
                         StringBuilder tmp = new StringBuilder();
                         tmp.append(currOrder).reverse();
                         order[cr][cc] = null;
@@ -148,6 +148,8 @@ public class Main {
                     updatePos(currOrder, oppositeR, oppositeC);
                 }
             }
+            
+            // 종료조건 체크했을때 달성시 턴 종료
             if(flag) break;
         }
 
@@ -156,6 +158,7 @@ public class Main {
         br.close();
     }
 
+		// 위치 업데이트 (쌓여있는 모든 말)
     private static void updatePos(String order, int nr, int nc){
         for(int j = 0; j < order.length(); j++) {
             int idx = Integer.valueOf(order.charAt(j)-'0');
