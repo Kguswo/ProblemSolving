090634b0419af5f43d1b8930c9309a0941e4138d
Now <156046839+Kguswo@users.noreply.github.com>
2025-09-24 10:21:48 +0900

    [level 2] Title: 구명보트, Time: 50.71 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
 
 ### 성능 요약
 
-메모리: 53.8 MB, 시간: 23.52 ms
+메모리: 59.5 MB, 시간: 50.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 13:33:49
+2025년 09월 24일 10:21:46
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,19 +1,25 @@
 import java.util.*;
 
 class Solution {
-    static List<Integer> list;
     public int solution(int[] people, int limit) {
-        int answer = 0;
+        List<Integer> list = new ArrayList<>();
         Arrays.sort(people);
-        list = new ArrayList<>();
-        for (int a : people) list.add(a); 
-        while (!list.isEmpty()) {
-            if (list.size() >= 2 && list.get(0) + list.get(list.size() - 1) <= limit) { // 젤 가벼운애 한명 더 태울 수 있으면 같이태움
-                list.remove(0);
-                list.remove(list.size() - 1);
-            } else list.remove(list.size() - 1); // 한명만 태울때
-            answer++;
+        for(int p : people){
+            list.add(p);
         }
-        return answer;
+        
+        int res = 0;
+        // (제일 무거운 alpha) or (alpha + 가벼운 beta 추가)
+        while(!list.isEmpty()){
+            if(list.size()>=2 && (list.get(list.size() - 1) + list.get(0)) <= limit){
+                list.remove(0);
+                list.remove(list.size()- 1);
+            }
+            else list.remove(list.size()-1);
+            
+            res++;
+
+        }
+        return res;
     }
 }
