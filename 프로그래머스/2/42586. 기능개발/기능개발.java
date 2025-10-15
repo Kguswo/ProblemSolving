52455d04daaa28c45ad59455ade2b88c08af0808
Now <156046839+Kguswo@users.noreply.github.com>
2025-10-15 10:27:48 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,42 +1,38 @@
 import java.util.*;
-import java.lang.Math;
+import java.lang.*;
 
 class Solution {
-    static int length;
+    static int len;
     public int[] solution(int[] progresses, int[] speeds) {
-        length = progresses.length;
-        int [] arr = new int[length];
+        len = progresses.length;
+        
+        int[] arr = new int[len];
         
         if((100-progresses[0]) % speeds[0] == 0) arr[0] = (100-progresses[0]) / speeds[0];
         else arr[0] = (100-progresses[0]) / speeds[0] +1;
         
-        for(int i=1; i<length; i++){
-            int value = (100-progresses[i]) / speeds[i];
-            if((100-progresses[i]) % speeds[i] == 0) arr[i] = Math.max(value, arr[i-1]);
-            else arr[i] = Math.max(value+1, arr[i-1]);
+        for(int i=1; i<len; i++){
+            int tmp = (100-progresses[i]) / speeds[i];
+            if((100-progresses[i]) % speeds[i] == 0) arr[i] = Math.max(tmp, arr[i-1]);
+            else arr[i] = Math.max(1 + tmp, arr[i-1]);
         }
+        
         int[] count = new int[101];
+        
         for(int a : arr){
             count[a]++;
         }
-        return Arrays.stream(count).filter(i -> i!=0).toArray();
-
         
-//         Set<Integer> sizecheck = new HashSet<>();
-//         for (int a : arr) sizecheck.add(a);
-//         int size = sizecheck.size();
+        List<Integer> list = new ArrayList<>();
+        for(int c : count){
+            if(c != 0) list.add(c);
+        }
         
-//         int[] answer = new int[size];
-//         int idx = 0;
-//         int cnt = 1;
-//         for (int i = 1; i < length; i++) {
-//             if (arr[i] == arr[i - 1]) cnt++;
-//             else {
-//                 answer[idx++] = cnt;
-//                 cnt = 1;
-//             }
-//         }
-//         answer[size-1] = cnt;
-//         return answer;
+        int[] res = new int[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        
+        return res;
     }
 }
