50188b849b3bb39654780589385dcc0151ade511
Now <156046839+Kguswo@users.noreply.github.com>
2025-12-24 17:48:22 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Solution {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
